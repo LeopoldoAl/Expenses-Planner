381014bba01cbe55369f219b669c35b780972881
Leopoldo <leopoldoalvaradogomez@gmail.com>
2024-10-07 12:33:16 -0400

    step 40

--- src/components/ExpenseForm.tsx
+++ src/components/ExpenseForm.tsx
@@ -1,5 +1,11 @@
-import { categories } from "../data/categories";
+import { categories } from "../data/categories"
+import DatePicker from 'react-date-picker'
+import 'react-calendar/dist/Calendar.css'
+import 'react-date-picker/dist/DatePicker.css'
 
+type ValuePiece = Date | null;
+
+type Value = ValuePiece | [ValuePiece, ValuePiece];
 
 export default function ExpenseForm() {
   return (
@@ -68,6 +74,20 @@ export default function ExpenseForm() {
                     </option>
                 ))}
             </select>
+        </div>
+
+        
+        <div className="flex flex-col gap-2">
+            <label 
+                htmlFor="amount" 
+                className="text-xl"
+            >
+                Expense Date:
+            </label>
+
+            <DatePicker
+                className="bg-slate-100 p-2 border-0"
+            />
         </div>
 
         <input 
