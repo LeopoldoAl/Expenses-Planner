9de527b025f2368e5b15f6e6354a54824088e1c0
Leopoldo <leopoldoalvaradogomez@gmail.com>
2024-10-09 12:54:01 -0400

    step 53

--- src/reducers/budget-reducer.ts
+++ src/reducers/budget-reducer.ts
@@ -62,7 +62,8 @@ export const budgetReducer = (
 
         return {
             ...state,
-            expenses: [...state.expenses, expense]
+            expenses: [...state.expenses, expense],
+            modal: false
         }
     }
     return state
