c05ec4a884f8f3463e3dd60b3e1013097e5ec030
Leopoldo <leopoldoalvaradogomez@gmail.com>
2024-10-15 09:39:42 -0400

    step 87

--- src/reducers/budget-reducer.ts
+++ src/reducers/budget-reducer.ts
@@ -1,5 +1,5 @@
 import {v4 as uuidv4} from 'uuid'
-import { DraftExpense, Expense } from "../types"
+import { Category, DraftExpense, Expense } from "../types"
 
 
 export type BudgetActions = 
@@ -10,13 +10,15 @@ export type BudgetActions =
     {type: 'remove-expense', payload:{ id: Expense['id']}} |
     {type: 'get-expense-by-id', payload:{ id: Expense['id']}} |
     {type: 'update-expense', payload:{ expense: Expense}} | 
-    {type: 'reset-expense'}
+    {type: 'reset-expense'} |
+    {type: 'add-filter-category', payload:{id: Category['id']}} 
 
 export type BudgetState = {
     budget: number,
     modal: boolean,
     expenses: Expense[],
-    editingId: Expense['id']
+    editingId: Expense['id'],
+    currentCategory: Category['id']
 }
 
 const initialBudget = ():number => {
@@ -33,7 +35,8 @@ export const initialState: BudgetState = {
     budget: initialBudget(),
     modal: false,
     expenses: bringDataStoredInLocalStore(),
-    editingId: ''
+    editingId: '',
+    currentCategory: ''
 }
 
 const createExpense = (draftExpense: DraftExpense): Expense => {
@@ -114,6 +117,13 @@ export const budgetReducer = (
             ...state,
             expenses: [],
             budget: 0
+        }
+    }
+
+    if (action.type==='add-filter-category') {
+        return {
+            ...state,
+            currentCategory: action.payload.id
         }
     }
     return state
